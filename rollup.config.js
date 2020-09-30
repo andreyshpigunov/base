f2ab7bea96ec7e31c9898e98dfdd9cd72fa730e9
Andrey Shpigunov <andreyshpigunov@me.com>
2020-09-30 10:25:02 +0000

    Updated rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -6,11 +6,6 @@ import babel from 'rollup-plugin-babel';
 export default {
   input: 'src/js/base.js',
   output: [
-//    {
-//      file: 'js/base.js',
-//      format: 'iife',
-//      name: 'Base'
-//    },
     {
       file: 'js/base.js',
       format: 'iife',
